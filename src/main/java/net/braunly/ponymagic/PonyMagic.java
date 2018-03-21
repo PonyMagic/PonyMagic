3d26a03d1671f9c7911264d167dce2c2f9c743de
Braunly <braunly.brony@gmail.com>
2018-03-21 11:24:29 +0200

    Bump version.

--- src/main/java/net/braunly/ponymagic/PonyMagic.java
+++ src/main/java/net/braunly/ponymagic/PonyMagic.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 @Mod(modid = PonyMagic.MODID, version = PonyMagic.VERSION, useMetadata = true)
 public class PonyMagic {
 	public static final String MODID = "ponymagic";
-	public static final String VERSION = "1.4.3-alpha";
+	public static final String VERSION = "1.5.0-alpha";
 	public static final String CLIENTPROXY = "net.braunly.ponymagic.proxy.ClientProxy";
 	public static final String COMMONPROXY = "net.braunly.ponymagic.proxy.CommonProxy";
 
