5823cadd759f53b47a640dc66f077e8aa21e2f91
Andriy Kushnir (Orhideous) <me@orhideous.name>
2018-05-05 17:59:51 +0300

    Correct internal versioning

--- src/main/java/net/braunly/ponymagic/PonyMagic.java
+++ src/main/java/net/braunly/ponymagic/PonyMagic.java
@@ -19,10 +19,9 @@ import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
-@Mod(modid = PonyMagic.MODID, version = PonyMagic.VERSION, useMetadata = true)
+@Mod(modid = PonyMagic.MODID, useMetadata = true)
 public class PonyMagic {
 	public static final String MODID = "ponymagic";
-	public static final String VERSION = "1.6.0";
 	public static final String CLIENTPROXY = "net.braunly.ponymagic.proxy.ClientProxy";
 	public static final String COMMONPROXY = "net.braunly.ponymagic.proxy.CommonProxy";
 	@NonNull public static final MetricRegistry METRICS = new MetricRegistry();
