bb9549f8f994be7883ee0a01a41db1889ffd22f4
Braunly <braunly.brony@gmail.com>
2018-08-30 01:46:49 +0300

    Reset rework.

--- src/main/java/net/braunly/ponymagic/network/packets/ResetPacket.java
+++ src/main/java/net/braunly/ponymagic/network/packets/ResetPacket.java
@@ -5,8 +5,12 @@ import net.braunly.ponymagic.PonyMagic;
 import net.braunly.ponymagic.data.PlayerData;
 import net.braunly.ponymagic.data.PlayerDataController;
 import net.braunly.ponymagic.handlers.MagicHandlersContainer;
+import net.braunly.ponymagic.items.ItemResetBook;
+import net.braunly.ponymagic.items.ModItems;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IThreadListener;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -32,11 +36,27 @@ public class ResetPacket implements IMessage, IMessageHandler<ResetPacket, IMess
 			@Override
 			public void run() {
 				if (player != null) {
-					PlayerData playerData = PlayerDataController.instance.getPlayerData(player);
-					playerData.reset();
-					playerData.save();
-					MagicHandlersContainer.updatePlayerFlySpeed(player, 0);
-					MagicHandlersContainer.updatePlayerMaxStamina(player);
+					if (player.experienceLevel >= 30 || player.inventory.hasItemStack(new ItemStack(ModItems.resetBook))) {
+						if (player.inventory.hasItemStack(new ItemStack(ModItems.resetBook))) {
+							for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
+				                ItemStack itemstack = player.inventory.getStackInSlot(i);
+
+				                if (itemstack.getItem() instanceof ItemResetBook) {
+				                    player.inventory.deleteStack(itemstack);
+				                    break;
+				                }
+				            }
+						} else {
+							player.addExperienceLevel(-30);
+						}
+						PlayerData playerData = PlayerDataController.instance.getPlayerData(player);
+						playerData.reset();
+						playerData.save();
+						MagicHandlersContainer.updatePlayerFlySpeed(player, 0);
+						MagicHandlersContainer.updatePlayerMaxStamina(player);
+					} else {
+						player.sendMessage(new TextComponentTranslation("skill.reset.fail", ""));
+					}
 				}
 			}
 		});
