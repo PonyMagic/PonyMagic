963ed267ee4bf62346fdbeb57e3bccca4b751dd2
Braunly <braunly.brony@gmail.com>
2018-09-03 16:13:15 +0300

    Fix items registration.

--- src/main/java/net/braunly/ponymagic/handlers/RegistrationHandler.java
+++ src/main/java/net/braunly/ponymagic/handlers/RegistrationHandler.java
@@ -1,13 +1,16 @@
 package net.braunly.ponymagic.handlers;
 
+import net.braunly.ponymagic.PonyMagic;
 import net.braunly.ponymagic.items.ModItems;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@EventBusSubscriber
+@EventBusSubscriber(modid=PonyMagic.MODID)
 public class RegistrationHandler {
 	
 //	@SubscribeEvent
@@ -19,6 +22,7 @@ public class RegistrationHandler {
 		ModItems.register(event.getRegistry());
 	}
 	
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public static void registerModels(ModelRegistryEvent event) {
 		ModItems.registerModels();
