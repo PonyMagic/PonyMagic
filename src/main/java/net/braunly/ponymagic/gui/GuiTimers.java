1cd87fcca0dc75f343509378cf25f6ac3100783c
Braunly <braunly.brony@gmail.com>
2019-12-22 20:01:57 +0200

    Some timers gui pixel moving.

--- src/main/java/net/braunly/ponymagic/gui/GuiTimers.java
+++ src/main/java/net/braunly/ponymagic/gui/GuiTimers.java
@@ -39,7 +39,7 @@ public class GuiTimers extends GuiIngameForge {
 
         ScaledResolution resolution = event.getResolution();
 
-        int xPos = resolution.getScaledWidth() / 2 - 100;
+        int xPos = resolution.getScaledWidth() / 2 - 95;
         int yPos = resolution.getScaledHeight() - 36;
 
 //        GL11.glEnable(GL11.GL_BLEND);
@@ -58,9 +58,10 @@ public class GuiTimers extends GuiIngameForge {
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
             float f = 1.0F;
 
+            // Some distance between timer icons
             int x = xPos - 36 * c;
             if (c > 1) {
-                x -= 5;
+                x -= 5 * (c - 1) ;
             }
 
             // draw bg
@@ -78,7 +79,7 @@ public class GuiTimers extends GuiIngameForge {
             GlStateManager.pushMatrix();
             float scaleFactor = 2.0F;
             GlStateManager.scale(scaleFactor, scaleFactor, scaleFactor);
-            mc.fontRenderer.drawString("§l" + text, (int) ((x + 6 - mc.fontRenderer.getStringWidth(text) / 2) / scaleFactor), (int)((yPos + 20) / scaleFactor), 16777215);
+            mc.fontRenderer.drawString("§l" + text, (int) ((x + 6 - mc.fontRenderer.getStringWidth(text) / 2) / scaleFactor), (int)((yPos + 17) / scaleFactor), 16777215);
             GlStateManager.color(1, 1, 1, 1);
             GlStateManager.popMatrix();
 
