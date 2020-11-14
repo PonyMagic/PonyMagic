e8398388e660b0b3a069972c9b4f6fb7729101b7
Braunly <braunly.brony@gmail.com>
2020-11-14 22:04:17 +0200

    Slightly format quests gui text.

--- src/main/java/net/braunly/ponymagic/gui/GuiQuests.java
+++ src/main/java/net/braunly/ponymagic/gui/GuiQuests.java
@@ -4,23 +4,17 @@ import lombok.Getter;
 import me.braunly.ponymagic.api.PonyMagicAPI;
 import me.braunly.ponymagic.api.interfaces.IPlayerDataStorage;
 import net.braunly.ponymagic.PonyMagic;
-import net.braunly.ponymagic.client.KeyBindings;
 import net.braunly.ponymagic.network.packets.RequestPlayerDataPacket;
 import net.braunly.ponymagic.util.QuestGoalUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -61,13 +55,6 @@ public class GuiQuests {
         int yPos = 10;
         int yShift = 0;
 
-//        GL11.glEnable(GL11.GL_BLEND);
-//        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-//        GL11.glDisable(GL11.GL_LIGHTING);
-
-//        GlStateManager.enableBlend();
-
-
         for (Map.Entry<String, HashMap<String, Integer>> questEntry : this.playerData.getLevelData().getCurrentGoals().entrySet()) {
             String questName = I18n.format("quest." + questEntry.getKey() + ".name");
             String questString = I18n.format("gui.quest.quest", questName);
@@ -75,7 +62,7 @@ public class GuiQuests {
             GlStateManager.pushMatrix();
             float scaleFactor = 2.0F;
             GlStateManager.scale(scaleFactor, scaleFactor, scaleFactor);
-            mc.fontRenderer.drawString("§l" + questString, (int) (xPos / scaleFactor), (int)((yPos + yShift) / scaleFactor), 16777215);
+            mc.fontRenderer.drawStringWithShadow("§6§l" + questString, (int) (xPos / scaleFactor), (int)((yPos + yShift) / scaleFactor), 16777215);
             GlStateManager.color(1, 1, 1, 1);
             GlStateManager.popMatrix();
             yShift += 15;
@@ -89,7 +76,7 @@ public class GuiQuests {
 
                 GlStateManager.pushMatrix();
                 GlStateManager.scale(scaleFactor, scaleFactor, scaleFactor);
-                mc.fontRenderer.drawString("§l" + goalString, (int) (xPos / scaleFactor), (int)((yPos + yShift) / scaleFactor), 16777215);
+                mc.fontRenderer.drawStringWithShadow("§l" + goalString, (int) (xPos / scaleFactor), (int)((yPos + yShift) / scaleFactor), 16777215);
                 GlStateManager.color(1, 1, 1, 1);
                 GlStateManager.popMatrix();
                 yShift += 15;
