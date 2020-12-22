520e35da7b1c960b1818906c6d1c0eca40a27401
Braunly <braunly.brony@gmail.com>
2020-12-22 21:01:00 +0200

    Fix invalid itemStack on ore check.

--- src/main/java/net/braunly/ponymagic/quests/handlers/BlockBreakEventHandler.java
+++ src/main/java/net/braunly/ponymagic/quests/handlers/BlockBreakEventHandler.java
@@ -42,7 +42,7 @@ public class BlockBreakEventHandler {
         String questName = "break_block";
         playerData.getLevelData().decreaseGoal(questName, goalConfigKey);
 
-        if (OreDictUtils.getInstance().isOre(new ItemStack(block))) {
+        if (OreDictUtils.getInstance().isOre(block.getItem(event.getWorld(), event.getPos(), event.getState()))) {
             questName = "mine_ore";
             playerData.getLevelData().decreaseGoal(questName, goalConfigKey);
         }
