d2e7ea7f9420760aa9d5bbed67e2ea58b93257ae
Braunly <braunly.brony@gmail.com>
2021-05-03 18:45:51 +0300

    Fix use_item event (bow 'use' and apple eating).

--- src/main/java/net/braunly/ponymagic/quests/handlers/ItemUseEventHandler.java
+++ src/main/java/net/braunly/ponymagic/quests/handlers/ItemUseEventHandler.java
@@ -3,14 +3,13 @@ package net.braunly.ponymagic.quests.handlers;
 import me.braunly.ponymagic.api.PonyMagicAPI;
 import me.braunly.ponymagic.api.enums.EnumQuestGoalType;
 import me.braunly.ponymagic.api.interfaces.IPlayerDataStorage;
-import net.braunly.ponymagic.PonyMagic;
 import net.braunly.ponymagic.util.QuestGoalUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.ArrowLooseEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class ItemUseEventHandler {
     public ItemUseEventHandler() {
@@ -18,7 +17,7 @@ public class ItemUseEventHandler {
     }
 
     @SubscribeEvent(priority=EventPriority.NORMAL)
-    public void onItemUse(LivingEntityUseItemEvent.Start event) {
+    public void onItemUse(LivingEntityUseItemEvent.Finish event) {
         if (!(event.getEntity() instanceof EntityPlayer)) return;
 
         EntityPlayer player = (EntityPlayer) event.getEntity();
@@ -31,6 +30,27 @@ public class ItemUseEventHandler {
                 EnumQuestGoalType.ITEM,
                 itemStack.getItem().getRegistryName(),
                 itemStack.getItemDamage()
+        );
+
+        String questName = "use_item";
+        playerData.getLevelData().decreaseGoal(questName, goalConfigKey);
+
+        PonyMagicAPI.playerDataController.savePlayerData(playerData);
+    }
+
+    @SubscribeEvent(priority=EventPriority.NORMAL)
+    public void onBowUse(ArrowLooseEvent event) {
+        if (!(event.getEntity() instanceof EntityPlayer)) return;
+
+        EntityPlayer player = (EntityPlayer) event.getEntity();
+
+        if (player.world.isRemote) return;
+
+        IPlayerDataStorage playerData = PonyMagicAPI.playerDataController.getPlayerData(player);
+        String goalConfigKey = QuestGoalUtils.getConfigKey(
+                EnumQuestGoalType.ITEM,
+                event.getBow().getItem().getRegistryName(),
+                0
         );
 
         String questName = "use_item";
