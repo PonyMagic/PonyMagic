fb0d0b42b1f4eaaa0c39ef85d5def14048bd8d85
Braunly <braunly.brony@gmail.com>
2021-05-06 23:31:41 +0300

    Add SpellBook item.

---
+++ src/main/java/net/braunly/ponymagic/items/ItemSpellBook.java
@@ -0,0 +1,43 @@
+package net.braunly.ponymagic.items;
+
+import me.braunly.ponymagic.api.PonyMagicAPI;
+import me.braunly.ponymagic.api.interfaces.IPlayerDataStorage;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public class ItemSpellBook extends ItemBase{
+
+	private final String spellName;
+
+	public ItemSpellBook(String name, String spellName) {
+		super(name);
+		this.setUnlocalizedName(name + "_" + spellName);
+		setMaxStackSize(1);
+
+		this.spellName = spellName;
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		if (world.isRemote) {
+			return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+		}
+		IPlayerDataStorage playerData = PonyMagicAPI.playerDataController.getPlayerData(player);
+		if (playerData.getRace().hasSpell(this.spellName) &&
+			!playerData.getSkillData().isSkillLearned(this.spellName)
+		) {
+			playerData.getSkillData().upSkillLevel(this.spellName);
+
+			ItemStack itemStackInHand = player.getHeldItem(hand);
+			itemStackInHand.setCount(itemStackInHand.getCount() - 1);
+			PonyMagicAPI.playerDataController.savePlayerData(playerData);
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+	}
+
+}
--- src/main/java/net/braunly/ponymagic/items/ModItems.java
+++ src/main/java/net/braunly/ponymagic/items/ModItems.java
@@ -9,23 +9,30 @@ public class ModItems {
 
 	public static ItemResetBook resetBook;
 	public static ItemCloudInBottle cloudInBottle;
+
+	public static ItemSpellBook portalSpellBook;
 	
 	public static void init() {
 		resetBook = new ItemResetBook("reset_book");
 		cloudInBottle = new ItemCloudInBottle("cloud_in_bottle");
+
+		portalSpellBook = new ItemSpellBook("spell_book", "portal");
 	}
 
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.registerAll(
 				resetBook,
-				cloudInBottle
+				cloudInBottle,
+				portalSpellBook
 		);
 	}
 	
 	public static void registerModels() {
 		ModelLoader.setCustomModelResourceLocation(resetBook, 0, 
 				new ModelResourceLocation(resetBook.getRegistryName(), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(cloudInBottle, 0, 
+		ModelLoader.setCustomModelResourceLocation(cloudInBottle, 0,
 				new ModelResourceLocation(cloudInBottle.getRegistryName(), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(portalSpellBook, 0,
+				new ModelResourceLocation(portalSpellBook.getRegistryName(), "inventory"));
 	}
 }
